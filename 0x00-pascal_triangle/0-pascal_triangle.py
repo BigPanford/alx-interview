d73b4757cdc0648131dde32322c0bec7617b5637
BigPanford <cycasbbone@gmail.com>
2024-10-07 05:45:38 +0000

    bigpanford

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,7 @@
 
 def pascal_triangle(n):
     if n <= 0:
+        
         return []
     
     triangle = [[1]]  # Starting with the first row
@@ -11,14 +12,14 @@ def pascal_triangle(n):
     for i in range(1, n):
         prev_row = triangle[-1]
         row = [1]  # Every row starts with a 1
-        # Generate the values of the row based on the previous row
+        #Generate the values of the row based on the previous row
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
-        row.append(1)  # Every row ends with a 1
-        triangle.append(row)
-    
-    return triangle
-
-# Example usage:
-n = 5
-print(pascal_triangle(n))
+            row.append(1)  # Every row ends with a 1
+            triangle.append(row)
+            
+            return triangle
+        
+        #Example usage:
+        n = 5
+        print(pascal_triangle(n))
